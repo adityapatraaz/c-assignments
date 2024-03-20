dcfc3c3f3a31e42389d04c27940460da59ba1f07
adityapatraaz <105380767+adityapatraaz@users.noreply.github.com>
2024-03-20 02:35:28 -0500

    Delete largest.c

--- largest.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    int a, b, c;
-    printf("Enter three integers :\n");
-    scanf("%d%d%d", &a, &b, &c);
-    a > b ? (a > c ? printf("%d is largest", a) : printf("%d is largest", c)) : (b > c ? printf("%d is largest", b) : printf("%d is largest", c));
-}
