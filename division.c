2b5db0e56a5f82a4ef5d4ddbccb096d84fb1af49
adityapatraaz <105380767+adityapatraaz@users.noreply.github.com>
2024-03-20 05:14:26 -0500

    Delete division.c

--- division.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-    int a,b;
-    printf("Enter the dividend :");
-    scanf("%d",&a);
-    printf("Enter the divisor :");
-    scanf("%d",&b);
-    printf("Quotient is %d and remainder is %d",a/b,a%b);
-}
